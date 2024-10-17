3e0bbdeba1f01c346ec60027e24506f19d5f2a03
willGraham01 <ccaegra@ucl.ac.uk>
2024-10-17 13:06:45 +0100

    Basic from json test

--- tests/test_directory.py
+++ tests/test_directory.py
@@ -1,18 +1,12 @@
 from pathlib import Path
-from typing import Dict, Optional, Set
+from typing import Optional, Set
 
 import git
 import pytest
-from conftest import JsonData
 
 from assignment_submission_checker.directory import Directory
 from assignment_submission_checker.git_utils import switch_if_safe
 from assignment_submission_checker.utils import AssignmentCheckerError
-
-
-@pytest.fixture
-def test_dir_structure(test_data_dict: JsonData) -> Directory:
-    return Directory("root", test_data_dict["structure"], parent=None)
 
 
 def test_init(test_dir_structure: Directory):
@@ -61,63 +55,6 @@ def test_traverse(test_dir_structure: Directory) -> None:
 
     for expected_name, dir in zip(expected_order, test_dir_structure):
         assert expected_name == dir.name, "Out-of-order-iteration through file structure!"
-
-
-@pytest.fixture
-def file_structure(request) -> Dict[str, str]:
-    """
-    Creates a file structure consistent with the template.json assignment definition.
-
-    If the request.param is True, then the file structure created will be incorrect in the
-    following ways:
-
-    - "repo_name/c2.py" will not be present, but is compulsory.
-    - "repo_name/report/wrong_image.pdf" will be present, but is not expected.
-    - "repo_name/data/wrong_format.json" will be present, but is not expected.
-    """
-    if request.param:
-        return {
-            "c1.py": "import json\n",
-            "report/report.md": "# My Report\n",
-            "report/plot.png": "funny_cat_gif",
-            "report/wrong_image.pdf": "content",
-            "report/AIUsage.md": "# AI Usage File\n",
-            "data/custom_data.csv": "forename,surname\nWill,Graham",
-            "data/wrong_format.json": "{'foo': 'bar'}",
-        }
-    else:
-        return {
-            "c1.py": "import json\n",
-            "c2.py": "from typing import Any\n",
-            "report/report.md": "# My Report\n",
-            "report/plot.png": "funny_cat_gif",
-            "report/AIUsage.md": "# AI Usage File\n",
-            "data/custom_data.csv": "forename,surname\nWill,Graham",
-        }
-
-
-@pytest.fixture
-def setup_folder_structure(tmp_path: Path, file_structure: Dict[str, str]):
-    """
-    A file structure that - besides a missing GIT repo - is compatible with
-    the template.json assignment structure defined in the tests/data folder.
-    """
-    repo_folder = tmp_path / "repo_name"
-    data_folder = repo_folder / "data"
-    report_folder = repo_folder / "report"
-
-    # Create folder structure
-    repo_folder.mkdir(parents=False, exist_ok=True)
-    data_folder.mkdir(parents=True, exist_ok=True)
-    report_folder.mkdir(parents=True, exist_ok=True)
-
-    # Populate with files
-    for file, content in file_structure.items():
-        with open(repo_folder / file, "w") as f:
-            f.write(content)
-
-    # Run test
-    yield
 
 
 ### Need to check compare against file structures...
@@ -190,6 +127,7 @@ def test_check_files(
     assert unexpected == expected_unexpected, "Not all unexpected files were identified."
 
 
+# TSTK REFACTOR THE GIT SETUP STUFF INTO CONFTEST!!
 @pytest.mark.parametrize(
     ["file_structure", "git_repo_loc", "git_commit_work_to", "git_switch_to", "expected_error"],
     [
